5ad5a381414beb576fb055cfb46b3bdde049e63f
kiranuday13 <srikanth1075@gmail.com>
2021-07-20 18:40:00 +0530

    some changes in function

---
+++ mathtable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void table(int x)
+{
+	int i;
+	for(i=1;i<=10;i++)
+	printf("%d*%d=%d\n",x,i,x*i);
+}
+/*main()
+{
+	int a;
+	void table(int);
+	printf("enter any num\n");
+	scanf("%d",&a);
+	table(a);
+}*/ 
